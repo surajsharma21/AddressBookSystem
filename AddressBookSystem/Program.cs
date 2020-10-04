d075fb16376b5f9df4e5bc86837afc2a8511d0f7
Suraj <surajshrm000@gmail.com>
2020-10-04 20:42:50 +0530

    [Suraj] Add: Ability to add multiple address books

--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -1,13 +1,59 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AddressBookSystem
 {
     class Program
     {
+        public static Dictionary<string, AddressBook> AddressBookMap = new Dictionary<string, AddressBook>();
         static void Main(string[] args)
         {
-            AddressBook addressBook = new AddressBook();
             Console.WriteLine("Welcome To Address Book Program");
+            int choice;
+            string name;
+            do
+            {
+                Console.WriteLine("\nMenu : \n 1.Add New Address Book \n 2.Work On Existing Address Book \n 3.Exit");
+                choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter the Name of Address Book");
+                        name = Console.ReadLine();
+                        AddressBookMap.Add(name, new AddressBook());
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter the Name of Address Book you wish to Work On");
+                        name = Console.ReadLine();
+                        AddressBook addressBook = AddressBookMap[name];
+                        FillAddressBook(addressBook);
+                        break;
+                }
+            } while (choice != 3);
+        }
+
+        public static void SetContactDetails(Contact contact)
+        {
+            Console.WriteLine("Enter the First Name");
+            contact.FirstName = Console.ReadLine();
+            Console.WriteLine("Enter the Last Name");
+            contact.LastName = Console.ReadLine();
+            Console.WriteLine("Enter the Address");
+            contact.Address = Console.ReadLine();
+            Console.WriteLine("Enter the City Name");
+            contact.City = Console.ReadLine();
+            Console.WriteLine("Enter the State Name");
+            contact.State = Console.ReadLine();
+            Console.WriteLine("Enter the zip code");
+            contact.Zip = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Phone Number");
+            contact.PhoneNumber = long.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the email address");
+            contact.Email = Console.ReadLine();
+        }
+
+        public static void FillAddressBook(AddressBook addressBook)
+        {
             int choice;
             do
             {
@@ -54,26 +100,6 @@ namespace AddressBookSystem
                         break;
                 }
             } while (choice != 0);
-        }
-
-        public static void SetContactDetails(Contact contact)
-        {
-            Console.WriteLine("Enter the First Name");
-            contact.FirstName = Console.ReadLine();
-            Console.WriteLine("Enter the Last Name");
-            contact.LastName = Console.ReadLine();
-            Console.WriteLine("Enter the Address");
-            contact.Address = Console.ReadLine();
-            Console.WriteLine("Enter the City Name");
-            contact.City = Console.ReadLine();
-            Console.WriteLine("Enter the State Name");
-            contact.State = Console.ReadLine();
-            Console.WriteLine("Enter the zip code");
-            contact.Zip = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the Phone Number");
-            contact.PhoneNumber = long.Parse(Console.ReadLine());
-            Console.WriteLine("Enter the email address");
-            contact.Email = Console.ReadLine();
         }
     }
 }
