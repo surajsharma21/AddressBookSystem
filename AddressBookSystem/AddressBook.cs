9afff1dcf44386c5e1d8a89e06d7c19f9f15db19
Suraj <surajshrm000@gmail.com>
2020-10-04 20:14:05 +0530

    [Suraj] Add: Creating Contact Class

---
+++ AddressBookSystem/AddressBook.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookSystem
+{
+    class AddressBook
+    {
+        public List<Contact> ContactList;
+        public AddressBook()
+        {
+            this.ContactList = new List<Contact>();
+        }
+    }
+}
